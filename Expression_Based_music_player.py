049ea4166a074f36eb83f8c275b77502fa57a4c4
Arun <arunv4080@gmail.com>
2018-06-06 16:01:28 +0530

    Expression based music player

--- Expression_Based_music_player.py
+++ Expression_Based_music_player.py
@@ -1,8 +1,33 @@
 import cv2
 import numpy as np
 import playsound
+import os, random
+from threading import Thread
 from keras.models import load_model
+
 faceCascade = cv2.CascadeClassifier('dataset/haarcascade_frontalface_alt2.xml')
+
+def Play_Music(path):
+    print(path)
+    playsound.playsound(path)
+
+def emoji(exp,Ex):
+
+    if Ex==exp:
+        a='emoji/'+exp+'.png'
+        
+    else:
+        t=random.choice(os.listdir("music/"+exp+"//")) #play random song
+        a='emoji/'+exp+'.png'
+        path='music/'+exp+'/'+t
+
+        if path !="":
+            t=Thread(target=Play_Music, args = [path])
+            t.deamon=True
+            t.start()
+
+    vc = cv2.imread(a)
+    return(vc)
 
 video_capture = cv2.VideoCapture(0)
 model = load_model('dataset/experssion_detector.hdf5')
@@ -10,8 +35,10 @@ model = load_model('dataset/experssion_detector.hdf5')
 target = ['angry','disgust','fear','happy','sad','surprise','neutral']
 font = cv2.FONT_HERSHEY_SIMPLEX
 
+
+Ex=''
 while True:
-    # Capture frame-by-frame
+    # Capture frame-by-frame 
     ret, frame = video_capture.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -29,47 +56,16 @@ while True:
         face_crop = face_crop.reshape(1, 1,face_crop.shape[0],face_crop.shape[1])
         result = target[np.argmax(model.predict(face_crop))]
         cv2.putText(frame,result,(x,y), font, 1, (200,100,0), 3, cv2.LINE_AA)
-        #print(result)
+        frame1 = emoji(result,Ex)
+        Ex=result
+        cv2.imshow('emoji', frame1)  # Display the resulting frame
+    k=cv2.waitKey(2000)
 
-		
-    # Display the resulting frame
-    cv2.imshow('emoji', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if k==27 & 0xFF == ord('q'):
         break
 
 # When everything is done, release the capture
 video_capture.release()
 cv2.destroyAllWindows()
 
-def emoji(exp):
-    a=''
-    if exp=='angry':
-        a=''
-        playsound.playsound(path)
 
-    elif exp=='disgust':
-        a=''
-        playsound.playsound(path)
-
-    elif exp=='fear':
-        a=''
-        playsound.playsound(path)
-
-    elif exp=='happy':
-        a=''
-        playsound.playsound(path)
-
-    elif exp=='sad':
-        a=''
-        playsound.playsound(path)
-
-    elif exp=='surprise':
-        a=''
-        playsound.playsound(path)
-
-    elif exp=='neutral':
-        a=''
-        playsound.playsound(path)
-        
-    return(a)
